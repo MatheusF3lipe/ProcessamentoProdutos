e54765fec2bcca5882f271b87e1f91fdee87e238
MatheusF3lipe <matheusvld77@gmail.com>
2025-05-12 21:01:46 -0300

    Adicionar arquivos de projeto.

---
+++ ProcessamentoProdutosDomain/Domain/Pedido.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessamentoProdutosDomain.Domain
+{
+    public class Pedido
+    {
+        public string? ClienteId { get; set; }
+        public List<String>? Itens { get; set; }
+    }
+}
